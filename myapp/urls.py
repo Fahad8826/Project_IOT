f01583b10c0ce004c1678eae6acebfabf31b2245
fahadaaaaa <fahadrahman8826@gmail.com>
2025-03-12 16:59:25 +0530

    Second commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,11 +1,11 @@
 from  django.urls import path
 
-from myapp.views import UserCreate, UserReadUpdate
+from myapp.views import UserCreate, UserReadUpdate, UserDelete
 
 # from myapp.views import home
 
 urlpatterns =[
-
     path('create',UserCreate.as_view(),name='create'),
-    path('read/',UserReadUpdate.as_view(),name='read'),
+    path('read/<int:pk>/',UserReadUpdate.as_view(),name='read'),
+    path('delete/<int:pk>/',UserDelete.as_view(),name ='delete'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -5,14 +5,16 @@ from .serializers import UserSerializers
 
 
 # Create your views here.
-# def home(request):
-#     users=Users.objects.all()
-#     return render(request,'home.html',{'users':users})
+
 
 class UserCreate(generics.ListCreateAPIView):
     queryset = Users.objects.all()
     serializer_class = UserSerializers
 
 class UserReadUpdate(generics.RetrieveUpdateAPIView):
+    queryset = Users.objects.all()
+    serializer_class = UserSerializers
+
+class UserDelete(generics.DestroyAPIView):
     queryset = Users.objects.all()
     serializer_class = UserSerializers
